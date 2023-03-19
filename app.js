2ba69a067612ff28280e81a4602436a3721d7db1
Kateryna Berehovych <kasia-94@ukr.net>
2023-03-19 12:01:27 +0200

    Add server

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+const contactsRouter = require("./routes/api/contacts");
+const indexRouter = require("./routes/api/index");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/contacts", contactsRouter);
+app.use("/api", indexRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
