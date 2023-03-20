44d166a3f14568b1fbd4dd9333a4844cde634265
Kateryna Berehovych <kasia-94@ukr.net>
2023-03-20 19:25:53 +0200

    Add db with docker

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 3000;
+const indexRouter = require("./routes/api/index");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello POSTGRES + NODEJS!!!");
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api", indexRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
 });
 
-app.listen(PORT, () => console.log(`Server started on post ${PORT}`));
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
